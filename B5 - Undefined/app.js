c345273d996f2c7918bea31bd1586b0653c3b2eb
sukh <sukhveer.sangha@tatacommunications.com>
2019-10-08 16:47:19 +0100

    exploring undefined

---
+++ B5 - Undefined/app.js
@@ -0,0 +1,13 @@
+
+var a;
+// undefined...variable not set
+// never set a variable to undefined either
+console.log(a); 
+
+if (a === undefined){
+    console.log('a is undefined.')
+} else {
+    console.log('a is defined')
+}
+// undefined a special value
+// different to not defined
